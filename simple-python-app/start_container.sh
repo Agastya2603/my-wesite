d52af9eb6573423f279957e8f2d4d504b9e18be0
Agastya <agastyag.2603@gmail.com>
2025-11-29 01:54:36 +0530

    Update start_container.sh
    add the files for code deploy

--- simple-python-app/start_container.sh
+++ simple-python-app/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull iamsandeepg/simple-python-flask-app1
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 iamsandeepg/simple-python-flask-app1
